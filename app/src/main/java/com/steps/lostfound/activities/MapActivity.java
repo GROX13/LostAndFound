6383e9ebd15232760959ac66220c054ea2f2e36c
Giorgi <giorgirokhadze@gmail.com>
2015-07-12 15:05:34 +0400

    Update Manifest

--- app/src/main/java/com/steps/lostfound/activities/MapActivity.java
+++ app/src/main/java/com/steps/lostfound/activities/MapActivity.java
@@ -1,19 +1,21 @@
 package com.steps.lostfound.activities;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
-/**
- * TODO
- * Created by Giorgi on 7/12/2015.
- */
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // TODO
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 
 }
